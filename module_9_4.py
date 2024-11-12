8a007e94669001b8361cdc65109386f5e7e8f541
drNorman <71461772+drNorman@users.noreply.github.com>
2024-11-12 20:58:20 +0300

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,42 @@
+from random import choice
+
+# ---------------------------------Lambda-функция------------------------
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+# -----------------------------------Замыкание---------------------------
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(f'{str(data)}\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# ---------------------------------Метод __call__------------------------
+
+class MysticBall:
+    words = []
+
+    def __init__(self, *words):
+        for word in words:
+            self.words.append(word)
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
